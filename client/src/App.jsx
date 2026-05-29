0d7d209b3ff7ebef6418531e77d6d6f93e98e27c
k-kushagrashukla <kushagra.shukla396@gmail.com>
2026-05-29 18:15:25 +0530

    adding frontend to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   const analyzeRepo = async (repoUrl) => {
     try {
-      const res = await axios.post('http://localhost:5000/api/analyze', {
+      const res = await axios.post('https://repolens-ai-zlr6.onrender.com/api/analyze', {
         repoUrl,
       })
 
